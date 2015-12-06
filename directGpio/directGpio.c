9c730d4ff1d4392db5bc8345e988ea796af541c2
Carl Peto <carl@petosoft.com>
2015-12-06 01:56:34 +0000

    add help

--- directGpio/directGpio.c
+++ directGpio/directGpio.c
@@ -6,9 +6,15 @@
 
 void sigInt(int signal);
 bool keepRunning = true;
-useconds_t mark = 1000;
-useconds_t space = 3000;
+useconds_t mark = 1;
+useconds_t space = 3;
 int pin = 21;
+
+void showHelp(char * basename) {
+  printf("Usage: %s [parameters]\n");
+  printf("-s <mark-space ratio>, fraction, how many spaces per mark as a multiple, default is 3\n");
+  printf("-p <pin>, 0-25, default is 21\n");
+}
 
 int main(int argc,char **argv)
 {
@@ -20,21 +26,30 @@ int main(int argc,char **argv)
     return -1;
   }
 
-  for (int a=1;a<argc-1;a+=2) {
-    if (a+1>=argc) break;
-    if (strncmp(argv[a],"-p",2)==0) {
-      // read the pin
-      printf("Using pin number %s\n",argv[a+1]);
-      pin = atoi(argv[a+1]);
-      if (pin < 0 || pin > 27) {
-        pin = 21;
+  if (argc>1&&strncmp(argv[1],"-h",2)==0) {
+    showHelp(argv[0]);
+    return 0;
+  } else {
+    for (int a=1;a<argc-1;a+=2) {
+      if (a>=argc-1) {
+        showHelp(argv[0]);
+        return 0;
       }
-    } else
-    if (strncmp(argv[a],"-s",2)==0) {
-      float spaceRatio = atof(argv[a+1]);
-      if (!isnan(spaceRatio) && spaceRatio > 0) {
-        space = spaceRatio * mark;
-	printf("mark %d, space %d\n",mark,space);
+      if (strncmp(argv[a],"-p",2)==0) {
+        // read the pin
+        printf("Using pin number %s\n",argv[a+1]);
+        pin = atoi(argv[a+1]);
+        if (pin < 0 || pin > 27) {
+          pin = 21;
+        }
+      } else if (strncmp(argv[a],"-s",2)==0) {
+        float spaceRatio = atof(argv[a+1]);
+        if (!isnan(spaceRatio) && spaceRatio > 0) {
+          space = spaceRatio * mark;
+	  printf("mark %d, space %d\n",mark,space);
+        } else {
+          printf("cannot interpret space ratio %0.2f\n",spaceRatio);
+        }
       }
     }
   }
