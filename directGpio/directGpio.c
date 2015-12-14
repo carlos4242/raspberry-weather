853935833351b0eb71e425c860f4c450dcf21eec
Carl Peto <carl.peto@me.com>
2015-12-14 03:35:39 +0000

    proper thread separation

--- directGpio/directGpio.c
+++ directGpio/directGpio.c
@@ -7,6 +7,7 @@
 #include <assert.h>
 
 #define CMD_BUF 1024
+#define NUM_PINS 25
 #define flasherPipe "/var/run/flasher"
 
 void sigInt(int signal);
@@ -18,31 +19,44 @@ void sigInt(int signal) {
 
 void showHelp(char * basename) {
   printf("Usage: echo s:XX:YYY > %s\n",flasherPipe);
-  printf("XX - pin number 0-25\n");
+  printf("XX - pin number 0-%d\n",NUM_PINS);
   printf("YYY - brightness 0-255\n");
 }
 
 const useconds_t dutyCycle = 256;
-useconds_t mark = 1;
-useconds_t space = 255;
-const unsigned char pin = 21;
+// useconds_t mark = 1;
+// useconds_t space = 255;
+// const unsigned char pin = 21;
 
-void *flasher(void *argument)
+// one thread for each pin to be PWMed
+pthread_t flashers[NUM_PINS];
+useconds_t marks[NUM_PINS] = {0};
+useconds_t spaces[NUM_PINS] = {0};
+
+void *flasher(void *pptr)
 {
+  unsigned char pin = *((unsigned char*)pptr);
+  printf("started thread for pin %d (at %p)\n",pin,pptr);
   while(keepRunning)
   {
-    if (mark) {
-      GPIO_SET = 1 << pin;
-      usleep(mark);
-    }
+    useconds_t mark = marks[pin];
+    useconds_t space = spaces[pin];
+    if (!mark && !space) {
+      usleep(dutyCycle);
+    } else {
+      if (mark) {
+        GPIO_SET = 1 << pin;
+        usleep(mark);
+      }
 
-    if (space) {
-      GPIO_CLR = 1 << pin;
-      usleep(space);
+      if (space) {
+        GPIO_CLR = 1 << pin;
+        usleep(space);
+      }
     }
   }
 
-  GPIO_CLR = 1 << 26 -1;
+  GPIO_CLR = 1 << pin;
  
   return NULL;
 }
@@ -65,9 +79,9 @@ void doControlMessage(char * message) {
         message += 3;
         unsigned char brightness = atoi(message);
         printf("message is %s, interpreted as brightness %d\n",message,brightness);
-        mark = brightness;
-        space = dutyCycle - mark;
-        printf("mark %d, space %d\n",mark,space);
+        marks[pin] = brightness;
+        spaces[pin] = dutyCycle - brightness;
+        // printf("mark %d, space %d\n",mark,space);
       }
     } else {
       printf("invalid length control message - should be s:XX:YYY was %d - %s\n",strnlen(message,6),message);
@@ -91,6 +105,8 @@ void *flasherctl(void *arg) {
 
 int main(int argc,char **argv)
 {
+  const unsigned char activePins[] = {21,25};
+
   signal(SIGINT,sigInt);
 
   if(map_peripheral(&gpio) == -1) 
@@ -104,13 +120,22 @@ int main(int argc,char **argv)
     return 0;
   }
 
-  pthread_t flasher_thread,flasher_control_thread;
-  int result_code = pthread_create(&flasher_thread, NULL, flasher, NULL);
-  assert(0 == result_code);
+  // first create the fifo listener
+  pthread_t flasher_control_thread;
   result_code = pthread_create(&flasher_control_thread, NULL, flasherctl, NULL);
   assert(0 == result_code);
-
   printf("Listening on %s...\n",flasherPipe);
+
+  // then create one flasher for each pin we will use
+  unsigned char pinNumbers[NUM_PINS];
+  for (unsigned char pin = 0;pin<NUM_PINS;pin++) {
+    if (pin == activePins[0] || pin == activePins[1]) {
+      pinNumbers[pin] = pin;
+      result_code = pthread_create(&flashers[pin], NULL, flasherctl, (void*)&pinNumbers[pin]);
+      printf("created thread for pin %d\n",pinNumbers[pin]);
+      assert(0 == result_code);
+    }
+  }
 
   while(keepRunning) {
   }
