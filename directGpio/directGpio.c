b44c6e59d45076553539eab7e3edbb8eb8d489c3
Carl Peto <carl.peto@me.com>
2015-12-14 02:17:37 +0000

    full pin brightness handling

--- directGpio/directGpio.c
+++ directGpio/directGpio.c
@@ -6,58 +6,87 @@
 #include <pthread.h>
 #include <assert.h>
 
+#define CMD_BUF 1024
+#define flasherPipe "/var/run/flasher"
+
 void sigInt(int signal);
 bool keepRunning = true;
-useconds_t mark = 1;
-useconds_t space = 3;
-int pin = 21;
-const char * flasherPipe = "/var/run/flasher";
+
+void sigInt(int signal) {
+  keepRunning = false;
+}
 
 void showHelp(char * basename) {
-  printf("Usage: %s [parameters]\n");
-  printf("-s <mark-space ratio>, fraction, how many spaces per mark as a multiple, default is 3\n");
-  printf("-p <pin>, 0-25, default is 21\n");
+  printf("Usage: echo s:XX:YYY > %s\n",flasherPipe);
+  printf("XX - pin number 0-25\n");
+  printf("YYY - brightness 0-255\n");
+}
+
+bool activePins[26] = { 0 };
+
+const useconds_t dutyCycle = 1000;
+unsigned char brightness = 100; // 0-255 
+useconds_t mark = 1;
+useconds_t space = dutyCycle - mark;
+const unsigned char pin = 21;
+
+void shutdownPins() {
+  for (int i = 0;i<26;i++) {
+    if (activePins[i]) {
+      GPIO_CLR = 1 << i;
+    }
+  }
 }
 
 void *flasher(void *argument)
 {
   while(keepRunning)
   {
-    // Toggle pin 7 (blink a led!)
-    GPIO_SET = 1 << pin;
-    usleep(mark);
- 
-    GPIO_CLR = 1 << pin;
-    usleep(space);
+    if (mark) {
+      GPIO_SET = 1 << pin;
+      usleep(mark);
+    }
+
+    if (space) {
+      GPIO_CLR = 1 << pin;
+      usleep(space);
+    }
   }
 
-  GPIO_CLR = 1 << pin;
+  shutdownPins(); // this could be done more efficiently with one call to clear the register but we'd need to poke into the innards of this helper library/macro... can't be bothered
  
   return NULL;
 }
 
-#define MAX_BUF 1024
-
 void doControlMessage(char * message) {
   printf("received control message %s\n",message);
-  if (strncmp(message,"s:",2)==0) {
+  if (strncmp(message,"s:",2)==0) { // structure is s:XX:YYY, where XX is the pin number and YYY is the brightness (0-255)
     message += 2;
-    message[10] = 0;
-    float spaceRatio = atof(message);
-    if (!isnan(spaceRatio) && spaceRatio > 0) {
-      space = spaceRatio * mark;
+    message[8] = 0;
+    if (strnlen(message,8)==8) {
+      char pinBuf[2];
+      strncpy(message,pinBuf,2);
+      unsigned char pin = atoi(pinBuf);
+      activePins[pin] = true;
+      // Define pin as output
+      INP_GPIO(pin);
+      OUT_GPIO(pin);
+      message += 3;
+      brightness = atoi(message);
+      mark = brightness ? dutyCycle / brightness : 0;
+      space = dutyCycle - mark;
       printf("mark %d, space %d\n",mark,space);
     } else {
-      printf("cannot interpret space ratio %0.2f\n",spaceRatio);
+      printf("invalid length control message - should be s:XX:YYY\n");
     }
   }
 }
 
 void *flasherctl(void *arg) {
-  char buf[MAX_BUF];
+  char buf[CMD_BUF];
   int flasherfd = open(flasherPipe,O_RDONLY);
   while (keepRunning) {
-    int r = read(flasherfd,buf,MAX_BUF);
+    int r = read(flasherfd,buf,CMD_BUF);
     if (r) {
       buf[r] = 0;
       doControlMessage(buf);
@@ -80,48 +109,18 @@ int main(int argc,char **argv)
   if (argc>1&&strncmp(argv[1],"-h",2)==0) {
     showHelp(argv[0]);
     return 0;
-  } else {
-    for (int a=1;a<argc-1;a+=2) {
-      if (a>=argc-1) {
-        showHelp(argv[0]);
-        return 0;
-      }
-      if (strncmp(argv[a],"-p",2)==0) {
-        // read the pin
-        printf("Using pin number %s\n",argv[a+1]);
-        pin = atoi(argv[a+1]);
-        if (pin < 0 || pin > 27) {
-          pin = 21;
-        }
-      } else if (strncmp(argv[a],"-s",2)==0) {
-        float spaceRatio = atof(argv[a+1]);
-        if (!isnan(spaceRatio) && spaceRatio > 0) {
-          space = spaceRatio * mark;
-	        printf("mark %d, space %d\n",mark,space);
-        } else {
-          printf("cannot interpret space ratio %0.2f\n",spaceRatio);
-        }
-      }
-    }
   }
 
-  // Define pin 7 as output
-  INP_GPIO(pin);
-  OUT_GPIO(pin);
-
   pthread_t flasher_thread,flasher_control_thread;
-
   int result_code = pthread_create(&flasher_thread, NULL, flasher, NULL);
   assert(0 == result_code);
   result_code = pthread_create(&flasher_control_thread, NULL, flasherctl, NULL);
   assert(0 == result_code);
 
+  printf("Listening on %s...\n",flasherPipe);
+
   while(keepRunning) {
   }
 
   return 0; 
-}
-
-void sigInt(int signal) {
-  keepRunning = false;
 }
