2b939bbedb46f72eb77ac2b67181ebfc8e010a39
Carl Peto <carl@petosoft.com>
2016-01-16 23:18:37 +0000

    tweaks

--- getWeather.js
+++ getWeather.js
@@ -62,7 +62,7 @@ function writeLights(cloudy,sunny,rain,alert,snow,hail,frost,chill,cloudCover,pp
 	}
 
 	if (frost) {
-		writableStream.write('s:12:255\n');
+		writableStream.write('s:12:020\n');
 	} else if (chill) {
 		writableStream.write('s:12:003\n');
 	} else {
@@ -89,7 +89,7 @@ function writeLights(cloudy,sunny,rain,alert,snow,hail,frost,chill,cloudCover,pp
 	}
 
 	if (alert) {
-		writableStream.write('f:23:005\n');
+		writableStream.write('f:23:025\n');
 	} else if (rain) {
 		var ps = pi * 10;
 		if (ps < 0) {
