9e36806ec869eb36864da4f26746dedbdb32d8f4
Carl Peto <carl.peto@me.com>
2016-04-07 16:01:53 +0100

    .

--- js/lights.js
+++ js/lights.js
@@ -1,32 +1,15 @@
 function refresh() {
-	$.get('/lights',function(data,status) {
+	$.get('/light',function(data,status) {
 		var json = $.parseJSON(data);
-		document.getElementById('lamp1').checked = !json[1]
-		document.getElementById('lamp2').checked = !json[2]
-		document.getElementById('lamp3').checked = !json[3]
+		document.getElementById('lamp1').value = json["light"]
 	});
 }
-function setLampOn(lamp,on) {
-	$.post('/lights/'+lamp,'on='+(on?0:1));
+function setLamp(lamp,value) {
+	$.post('/light','brightness='+value,function(){refresh()});
 }
 $(document).ready(function(){
 	$('#lamp1').prop('disabled', false).change(function(){
-		setLampOn(1,$(this).is(':checked'));
-	});
-	$('#lamp2').prop('disabled', false).change(function(){
-		setLampOn(2,$(this).is(':checked'));
-	});
-	$('#lamp3').prop('disabled', false).change(function(){
-		setLampOn(3,$(this).is(':checked'));
-	});
-	$('body').append($("<br><br><button id='allOnBtn' hidden>All On</button>   <button id='allOffBtn' hidden>All Off</button>"));
-	$('#allOnBtn').fadeIn('slow',function() {
-		$('#pageTitle').text('light control');
-	}).click(function(){
-		$.post('/lights','allOn=1',function(){refresh()});
-	});
-	$('#allOffBtn').fadeIn('slow').click(function(){
-		$.post('/lights','allOff=1',function(){refresh()});
+		setLampOn(1,$(this).value());
 	});
 	refresh();
 	setInterval(refresh, 10000);
--- server.js
+++ server.js
@@ -25,35 +25,20 @@ app.get('/', function(req, res) {
 	i2c1.i2cReadSync(0x04,1,b);
 	i2c1.closeSync();
 
-	console.log('read lights as '+b[0]);
+	console.log('read light as '+b[0]);
 
   var params = {
-    tubeLampOn:(b[0]&2?"":"checked"),
-    roundLampOn:(b[0]&1?"":"checked"),
-    cornerLampOn:(b[0]&4?"":"checked")
+  	lampBrightness:b[0]
   };
   res.render('index',params);
 });
 
 // GET /   ... get web code
-// GET /lights   ... get json of lights status
-// GET /lights/light1   ...   get json of single light status
-// POST /lights  ...  allOn=1 ... turn all lights on and return json of lights status
-// POST /lights  ...  allOff=1 ... turn all lights off and return json of lights status
-// POST /lights/light2  ...  on=x  ... turn lights on or off (1 and 0 are only recognized values) returns json of single light status
+// GET /light   ... get json of light status
+// POST /light  ...  brightness=XX ... change light brightness
 
 function lights(lightsByte,lightNo) {
-	// bit 0 is light 1, bit 1 is light 2, bit 2 is light 3
-	// in each case 1 means high or off
-	if (lightNo == undefined) {
-		return {"1":(lightsByte&1?1:0),"2":(lightsByte&2?1:0),"3":(lightsByte&4?1:0)}
-	} else if (lightNo == "1") {
-		return lightsByte&1?1:0;
-	} else if (lightNo == "2") {
-		return lightsByte&2?1:0;
-	} else if (lightNo == "3") {
-		return lightsByte&4?1:0;
-	}
+	return {"light":lightsByte}
 }
 
 function getLightsReply(req,res) {
@@ -64,60 +49,17 @@ function getLightsReply(req,res) {
 	res.end(JSON.stringify(lights(b[0])));
 }
 
-app.get('/lights',getLightsReply);
+app.get('/light',getLightsReply);
 
-app.post('/lights',function(req,res) {
+app.post('/light',function(req,res) {
 	console.log('post to lights');
 	var i2c1 = i2c.openSync(1);
-	if (req.body.allOn == 1) {
-		i2c1.i2cWriteSync(0x04,1,new Buffer("1"));
-	} else if (req.body.allOff == 1) {
-		i2c1.i2cWriteSync(0x04,1,new Buffer("0"));
-	}
+	var br = req.body.brightness;
+	i2c1.i2cWriteSync(0x04,1,new Buffer(br));
+
 	i2c1.closeSync();
 	setTimeout(function() {
 		// give the arduino time before the next i2c bus request
 		getLightsReply(req,res);
 	},100);
-});
-
-app.post('/lights/:light',function(req,res) {
-	console.log('post to light '+req.params.light);
-	console.log('on = '+req.body.on);
-	var i2c1 = i2c.openSync(1);
-	var cmd = null;
-	switch (req.params.light) {
-		case "1":
-			cmd = (req.body.on == 1)?"a":"b";
-			console.log("light1");
-			break;
-		case "2":
-			cmd = (req.body.on == 1)?"c":"d";
-			console.log("light2");
-			break;
-		case "3":
-			cmd = (req.body.on == 1)?"e":"f";
-			console.log("light3");
-			break;
-	}
-	console.log("cmd : "+cmd);
-	if (cmd) {
-		i2c1.i2cWriteSync(0x04,1,new Buffer(cmd));
-	}
-	i2c1.closeSync();
-	setTimeout(function() {
-		// give the arduino time before the next i2c bus request
-		getLightsReply(req,res);
-	},100);
-});
-
-app.get('/lights/:light',function(req,res) {
-	var i2c1 = i2c.openSync(1);
-	var b = new Buffer(1);
-	i2c1.i2cReadSync(0x04,1,b);
-	i2c1.closeSync();
-	var light = req.params.light;
-	var result = {};
-	eval('result = {\"'+light+'\":'+lights(b[0],light)+'}');
-	res.end(JSON.stringify(result));
 });
