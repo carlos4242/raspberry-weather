c105a9c00bf88ce021464fb6d9fd7636c3fed69d
Carl Peto <carl.peto@me.com>
2016-04-13 11:41:43 +0100

    updates

--- server.js
+++ server.js
@@ -19,47 +19,54 @@ app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/res',express.static(__dirname + '/res'));
 
-app.get('/', function(req, res) {
-	var i2c1 = i2c.openSync(1);
-	var b = new Buffer(1);
-	i2c1.i2cReadSync(0x04,1,b);
-	i2c1.closeSync();
+// we communicate with the Arduino via I2C on bus one, device 4
+// dimming level (0-128)  0 = on, 128 = off
 
-	console.log('read light as '+b[0]);
-
-  var params = {
-  	lampBrightness:b[0]
-  };
-  res.render('index',params);
-});
 
 // GET /   ... get web code
 // GET /light   ... get json of light status
 // POST /light  ...  brightness=XX ... change light brightness
 
-function lights(lightsByte,lightNo) {
-	return {"light":lightsByte}
-}
+// to make it more friendly, we will reverse the dimming level so 0 is off and 128 is maximum brightness
+// any number below 0 will be read as 0 and any number above 128 will be read as 128
 
-function getLightsReply(req,res) {
+app.get('/', function(req, res) {
 	var i2c1 = i2c.openSync(1);
 	var b = new Buffer(1);
 	i2c1.i2cReadSync(0x04,1,b);
 	i2c1.closeSync();
-	res.end(JSON.stringify(lights(b[0])));
-}
+	var brightness = 128 - b[0];
+  var params = {
+  	lampBrightness:brightness
+  };
+  res.render('index',params);
+});
 
 app.get('/light',getLightsReply);
 
 app.post('/light',function(req,res) {
 	console.log('post to lights');
-	var i2c1 = i2c.openSync(1);
 	var br = req.body.brightness;
+	if (br<0) {
+		br = 0;
+	} else if (br>128) {
+		br = 128;
+	}
+	br = 128 - br;
+	var i2c1 = i2c.openSync(1);
 	i2c1.i2cWriteSync(0x04,1,new Buffer([br]));
-
 	i2c1.closeSync();
 	setTimeout(function() {
 		// give the arduino time before the next i2c bus request
 		getLightsReply(req,res);
 	},100);
 });
+
+function getLightsReply(req,res) {
+	var i2c1 = i2c.openSync(1);
+	var b = new Buffer(1);
+	i2c1.i2cReadSync(0x04,1,b);
+	i2c1.closeSync();
+	var brightness = 128 - b[0];
+	res.end(JSON.stringify(lights({"light":brightness})));
+}
