0f686b453b107e87177ecae480d6b688c249a3fa
Carl Peto <carl.peto@me.com>
2016-09-26 03:08:26 +0100

    use streams

--- server.js
+++ server.js
@@ -60,25 +60,141 @@ app.get('/weather.txt',function(req,res) {
 
 // callback takes two parameters, err and brightness, which is 0 if an error occurred
 function getBrightness(cb) {
-	console.log("opening /dev/ttyACM0...");
-	fs.open('/dev/ttyACM0', 'r+', function(err, fd) {
-		if (err) {
-			console.log("error opening usb port : "+err);
-			cb(err,0);
-		}
-		else {
-			console.log("opened /dev/ttyACM0...");
-			console.log("to write...");
-			// send the command request to the arduino
-			fs.write(fd,Buffer("DMR1:?"),0,6,0,function(err, written, string) {
-				if (err) {
-					console.log("error writing to usb port : "+err);
-				} else {
-					console.log("wrote /dev/ttyACM0...");
-				}
-			});
+	var readStream = fs.createReadStream('/dev/ttyACM0');
 
+	var closeStr = function(err,brightness) {
+		if (readStream) {
+			readStream.close();
+			readStream = false;
+		}
+		cb(err,brightness);
+	};
+
+	readStream.on('data', function(data) {
+		console.log('data : '+data);
+		if ((""+data).substring(0,5) == 'DMR1=') {
+			var dimmerValue = Number((""+data).substring(5,10));
+			console.log('dimmer value : '+(dimmerValue));
+		}
+		closeStr(null,dimmerValue);
+	});
+
+	// This catches any errors that happen while creating the readable stream (usually invalid names)
+	readStream.on('error', function(err) {
+		console.log('stream error : '+err);
+		closeStr(err,0);
+	});
+
+	// This will wait until we know the readable stream is actually valid before piping
+	readStream.on('open', function (fdread) {
+	  // This just pipes the read stream to the response object (which goes to the client)
+	  // readStream.pipe(res);
+	  console.log('opened pipe '+fdread);
+
+		// send the command request to the arduino
+		fs.open('/dev/ttyACM0', 'w', function(err, fdwrite) {
+			if (err) {
+				console.log("error opening usb port : "+err);
+				// cb(err,0);
+			} else {
+				console.log("opened /dev/ttyACM0...");
+				console.log("to write...");
+				var bytes = fs.writeSync(fdwrite,"DMR1:?");
+				if (bytes) {
+					console.log("wrote /dev/ttyACM0... bytes : "+bytes);
+				} else {
+					console.log("error writing to usb port : <unknown>");
+				}
+				fs.close(fdwrite);
+			}
+		});
+
+		setTimeout(closeStr,3);
+	});
+}
+
+function getLightsReply(req,res) {
+	getBrightness(function(err,brightness) {
+		if (err) {
+			res.writeHead(500);
+			res.end("Cannot get brightness "+err);
+		} else {
+			res.end(JSON.stringify({"light":brightness}));
+		}
+	});
+}
+
+
+
+
+
+	// console.log("opening /dev/ttyACM0...");
+	// fs.open('/dev/ttyACM0', 'r+', function(err, fd) {
+	// 	if (err) {
+	// 		console.log("error opening usb port : "+err);
+	// 		cb(err,0);
+	// 	}
+	// 	else {
+	// 		console.log("opened /dev/ttyACM0...");
+	// 		console.log("to write...");
+	// 		// send the command request to the arduino
+
+
+	// 		var bytes = fs.writeSync(fd,"DMR1:?");
+	// 		if (bytes) {
+	// 			console.log("wrote /dev/ttyACM0... bytes : "+bytes);
+	// 		} else {
+	// 			console.log("error writing to usb port : <unknown>");
+	// 		}
+
+	// 		// fs.write(fd,Buffer("DMR1:?"),0,6,0,function(err, written, string) {
+	// 		// 	if (err) {
+	// 		// 		console.log("error writing to usb port : "+err);
+	// 		// 	} else {
+	// 		// 		console.log("wrote /dev/ttyACM0...");
+	// 		// 	}
+	// 		// });
+
+	// 	  var readStream = fs.createReadStream('/dev/ttyACM0');
+
+	// 	  // This will wait until we know the readable stream is actually valid before piping
+	// 	  readStream.on('open', function () {
+	// 	    // This just pipes the read stream to the response object (which goes to the client)
+	// 	    // readStream.pipe(res);
+	// 	    console.log('opened pipe');
+	// 	  });
+
+	// 	  req.on('data', (chunk) => {
+	// 	  	console.log('data : '+chunk);
+	// 	    // body += chunk;
+	// 	  });
+
+	// 	  // This catches any errors that happen while creating the readable stream (usually invalid names)
+	// 	  readStream.on('error', function(err) {
+	// 	  	cb(err,0);
+	// 	    // res.end(err);
+	// 	  });
+
+
+	// 	}
+	// });
 			// var buffer = Buffer(10);
+			// fs.readSync(fd,buffer,0,6,null);
+
+			// function(err, bytesRead, buffer) {
+			// 	console.log("rd");
+			// 	if (err) {
+			// 		console.log("error reading from usb port : "+err);
+			// 		cb(err,0);
+			// 	} else {
+			// 		console.log("read 6 from /dev/ttyACM0...");
+			// 		console.log(buffer); // should interpret as brightness
+			// 		cb(null,10);
+			// 	}
+			// 	fs.close(fd,null);
+			// });
+
+
 			// fs.read(fd,buffer,0,6,null,function(err, bytesRead, buffer) {
 			// 	console.log("rd");
 			// 	if (err) {
@@ -100,21 +216,6 @@ function getBrightness(cb) {
 			// 		console.log("wrote /dev/ttyACM0...");
 			// 	}
 			// });
-		}
-	});
-}
-
-function getLightsReply(req,res) {
-	getBrightness(function(err,brightness) {
-		if (err) {
-			res.writeHead(500);
-			res.end("Cannot get brightness "+err);
-		} else {
-			res.end(JSON.stringify({"light":brightness}));
-		}
-	});
-}
-
 
 // var i2c = require('i2c-bus');
 // it would be nice to make these constants when the language allows
